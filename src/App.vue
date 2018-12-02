ca64b3f7ddfd8d2b7f3ab18effb92d992825041f
michael <michael.tessier.paris@gmail.com>
2018-12-02 23:39:40 +0100

    add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Product</router-link> |
-      <router-link to="/cart">Cart</router-link>
-    </div>
-    <router-view/>
+    <NavBar />
+    <RouterView />
   </div>
 </template>
+
+<script>
+import NavBar from "@/components/NavBar"
+
+
+export default {
+  name: 'App',
+
+  components: {
+    NavBar
+  }
+
+}
+
+</script>
 
 <style lang="scss">
 @import "@/assets/scss/index.scss";
---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header id="nav">
+    <RouterLink to="/">
+      Product
+    </RouterLink> |
+    <RouterLink to="/cart">
+      Cart ({{ getTotalProduct }})
+    </RouterLink>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+  name: 'NavBar',
+  
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  
+  computed: {
+    ...mapGetters({
+      getTotalProduct: 'cart/getTotalProduct'
+    })
+  },
+}
+
+</script>
