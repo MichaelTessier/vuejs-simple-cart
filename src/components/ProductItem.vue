dfbf8f038f89839dbdf69a722e09349b1385db06
michael <michael.tessier.paris@gmail.com>
2018-12-02 18:09:02 +0100

    add productItem component

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,16 +1,58 @@
 <template>
-  <div class="co-product-item">
+  <article class="co-product-item">
+    <img 
+      :src="thumbnailUrl" 
+      :alt="title"
+    >
+    <h1>{{ productLabel }} {{ productId }}</h1>
+    <p>{{ title }}</p>
 
-    <h1>{{ msg }}</h1>
-    
-  </div>
+    <Button
+      :label="LabelButton"
+      @click="addToCart"
+    />
+  </article>
 </template>
 
 <script>
+
+import Button from "@/components/Button"
+import { method } from 'bluebird';
+
 export default {
   name: "ProductItem",
+
+  components: {
+    Button
+  },
+  
   props: {
-    msg: String
+    productId: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    thumbnailUrl: {
+      type: String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      buttonLabel: 'Add to cart',
+      productTitle: 'Product'
+    }
+  },
+
+  methods: {
+    addToCart() {
+      console.log('Add To Cart')
+    }
   }
+
 };
 </script>
