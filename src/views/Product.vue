1fa96ebf2efb7d569e17927ee743fe4396bb01f4
michael <michael.tessier.paris@gmail.com>
2018-12-02 18:07:07 +0100

    add products

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="vi-product">
-    <h1>This is an product page</h1>
-  </div>
+  <section class="vi-product">
+    <div v-if="products">
+      <h1>This is an product page</h1>
+
+      <ProductItem 
+        v-for="product in products"
+        :key="product.id"
+        :product-id="product.id"
+        :title="product.title"
+        :thumbnail-url="product.thumbnailUrl"
+      />
+    </div>
+    
+    <div v-if="!products">
+      Loading product
+    </div>
+  </section>
 </template>
+
+<script>
+import axios from "axios";
+import ProductItem from "@/components/ProductItem";
+
+
+export default {
+  name: "Product",
+
+  components: {
+    ProductItem
+  },
+
+  data() {
+    return {
+      title: 'Product',
+      products: null
+    };
+  },
+
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then(response => {
+        this.products = response.data.filter(product => product.albumId === 1);
+      });
+  }
+};
+</script>
